7627e9e29f607f5cfca1452de0b66b7d495e5524
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-06-18 23:01:53 +0100

    add additional weather information to display, change animation length

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 const form = document.getElementById('form');
 const location = document.getElementById('location');
+const resultsMain = document.getElementById('resultsMain');
 const resultsContainer = document.getElementById('resultsContainer');
 
 let weatherResults = []
-const results = document.createElement('div');
 
-function Weather(location, temperature) {
+const mainResultLocation = document.createElement('div');
+const mainResultTemperature = document.createElement('div');
+const mainResultCondition = document.createElement('div');
+
+const resultFeelsLike = document.createElement('div');
+const resultHumidity = document.createElement('div');
+const resultWind = document.createElement('div');
+
+function Weather(location, temperature, feelsLikeCelcius, humidity, wind, condition) {
   this.location = location;
   this.temperature = temperature;
-  this.infoCelcius = `The temperature in ${location} is ${temperature}` + "°.";
+  this.feelsLikeCelcius = feelsLikeCelcius;
+  this.humidity = humidity;
+  this.wind = wind;
+  this.condition = condition;
 }
 
-// takes search value and awaits fetching of data
-// creates a weather object with searched location and temperature
-// logs the object
 async function fetchWeather(searchValue) {
   const url = "https://api.weatherapi.com/v1/current.json?key=9e73d1b2999849e59b0205612231706&q=";
   const response = await fetch(url + searchValue, { mode: "cors" });
@@ -22,23 +30,54 @@ async function fetchWeather(searchValue) {
 
   let weatherLocation = weatherData.location.name;
   let weatherTemp = weatherData.current.temp_c;
-  let weather = new Weather(weatherLocation, weatherTemp);
+  let weatherFeelsLikeCelcius = weatherData.current.feelslike_c; 
+  let weatherHumidity = weatherData.current.humidity;
+  let weatherWind = weatherData.current.wind_mph;
+  let weatherCondition = weatherData.current.condition.text;
+  let weather = new Weather(weatherLocation, weatherTemp, weatherFeelsLikeCelcius, weatherHumidity, weatherWind, weatherCondition);
 
-  results.id = "results";
-  resultsContainer.appendChild(results);
+  resultsMain.appendChild(mainResultLocation);
+  resultsMain.appendChild(mainResultTemperature);
+  resultsMain.appendChild(mainResultCondition);
+
+  mainResultLocation.classList.add('mainResult');
+  mainResultTemperature.classList.add('mainResult');
+  mainResultCondition.classList.add('mainResult');
+
+  resultFeelsLike.id = "result1";
+  resultHumidity.id = "result2";
+  resultWind.id = "result3";
+  resultsContainer.appendChild(resultFeelsLike);
+  resultsContainer.appendChild(resultHumidity);
+  resultsContainer.appendChild(resultWind);
+
   console.log(weather);
   weatherResults.push(weather);
 
-  results.textContent = weatherResults[0].infoCelcius;
+  mainResultLocation.textContent = weatherResults[0].location;
+  mainResultTemperature.textContent = weatherResults[0].temperature + "°";
+  mainResultCondition.textContent = weatherResults[0].condition;
+
+  resultFeelsLike.textContent = `It feels like ${weatherResults[0].feelsLikeCelcius}°`;
+  resultHumidity.textContent = `Humidity: ${weatherResults[0].humidity}%`
+  resultWind.textContent = `Wind speed: ${weatherResults[0].wind} mph`
+
+  if (weatherResults[0].temperature <= 10){
+    mainResultTemperature.style.color = 'blue'
+  } else if (weatherResults[0].temperature >= 10){
+    mainResultTemperature.style.color = 'yellow'
+  }
 }
 
-function clearResults(){
+function clearResultsContainer(){
     weatherResults = [];
-    results.textContent = "";
+    resultFeelsLike.textContent = "";
+    resultHumidity.textContent = "";
+    resultWind.textContent = "";
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     fetchWeather(location.value).catch(e => { console.log(e) }); 
-    clearResults();
+    clearResultsContainer();
 })
