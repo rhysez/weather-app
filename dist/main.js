5c95105306442056cd569282914a683ce9f5ee3c
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-06-18 13:11:24 +0100

    initial commit, webpack set up

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Whats the weather like?')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ1doYXRzIHRoZSB3ZWF0aGVyIGxpa2U/JykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
